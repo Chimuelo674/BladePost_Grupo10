b4026de3c153a8941551197aa175b3aed0b26223
al.vegag@duocuc.cl <167357721+AlexisIsaiasVega@users.noreply.github.com>
2025-11-22 21:06:31 -0300

    se agrego confirmacion de contraseña y validaciones en el registro del usuario

--- app/src/main/java/com/example/bladepost_grupo10/MainActivity.kt
+++ app/src/main/java/com/example/bladepost_grupo10/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.bladepost_grupo10
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,23 +12,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface // Importación necesaria
-import androidx.compose.material3.TopAppBar
-import androidx.compose.ui.Alignment
-import androidx.navigation.NavHostController
 import com.example.bladepost_grupo10.ui.DetailScreen
 import com.example.bladepost_grupo10.ui.LoginScreen
 import com.example.bladepost_grupo10.ui.RegistroScreen
@@ -43,9 +27,6 @@ import androidx.compose.runtime.getValue // Importación necesaria
 import androidx.compose.runtime.mutableStateOf // Importación necesaria
 import androidx.compose.runtime.remember // Importación necesaria
 import androidx.compose.runtime.setValue // Importación necesaria
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 
 
 // 1. DEFINICIÓN DE RUTAS (Screens)
--- app/src/main/java/com/example/bladepost_grupo10/ui/RegistroScreen.kt
+++ app/src/main/java/com/example/bladepost_grupo10/ui/RegistroScreen.kt
@@ -25,35 +25,63 @@ fun RegistroScreen(navController: NavHostController){
     var usuario by remember { mutableStateOf("") }
     var correo by remember { mutableStateOf("") }
     var contrasena by remember { mutableStateOf("") }
+    var confirmarContrasena by remember { mutableStateOf("") }
 
     var isNameError by remember {mutableStateOf(false)}
     var isUserError by remember {mutableStateOf(false)}
     var isEmailError by remember {mutableStateOf(false)}
     var isPasswordError by remember {mutableStateOf(false)}
+    var isConfirmPasswordError by remember {mutableStateOf(false)}
+
+    val nameRegex = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+\$".toRegex()
+    val userRegex = "^[a-zA-Z0-9_]+\$".toRegex()
+    val passwordRegex = "^[a-zA-Z0-9\\p{Punct}\\s\\S&&[^áéíóúÁÉÍÓÚñÑ]]+\$".toRegex()
 
     val validaryregistrar:()->Unit ={
         isNameError = false
         isUserError = false
         isEmailError = false
         isPasswordError = false
+        isConfirmPasswordError = false
         var hasError = false
 
-        if(nombre.isEmpty()){
+        // 1. VALIDACIÓN DE NOMBRE COMPLETO
+        if(nombre.isEmpty() || !nombre.matches(nameRegex)){
             isNameError = true
             hasError = true
         }
-        if(usuario.isEmpty()){
+
+        // 2. VALIDACIÓN DE USUARIO
+        if(usuario.isEmpty() || !usuario.matches(userRegex)){
             isUserError = true
             hasError = true
         }
+
+        // 3. VALIDACIÓN DE CORREO
         if(correo.isEmpty()|| !correo.contains("@")){
             isEmailError = true
             hasError = true
         }
-        if(contrasena.length < 6){
-                isPasswordError = true
-                hasError = true
+
+        // 4. VALIDACIÓN DE CONTRASEÑA: Longitud Y Regex
+        if(contrasena.length < 6 || !contrasena.matches(passwordRegex)){
+            isPasswordError = true
+            hasError = true
         }
+
+        // 5. VALIDACIÓN DE CONFIRMACIÓN: Debe coincidir Y pasar la Regex
+        if(confirmarContrasena.isEmpty()){
+            isConfirmPasswordError = true
+            hasError = true
+        } else if (contrasena != confirmarContrasena){
+            isConfirmPasswordError = true
+            hasError = true
+        } else if (!confirmarContrasena.matches(passwordRegex)){
+            isConfirmPasswordError = true
+            hasError = true
+        }
+
+
         if (!hasError) {
             //verificar si el usuario o correo ya existen
             val existe = usuarioDao.verificarUsuarioExistente(usuario, correo)
@@ -86,100 +114,122 @@ fun RegistroScreen(navController: NavHostController){
             )
         }
     ){ paddingValues ->
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues)
-            .padding(horizontal = 24.dp, vertical = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 24.dp, vertical = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
 
-    ){
-        Text(
-            text = "Registro de Usuario",
-            style = MaterialTheme.typography.headlineSmall
-        )
+        ){
+            Text(
+                text = "Registro de Usuario",
+                style = MaterialTheme.typography.headlineSmall
+            )
 
-        Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(24.dp))
 
-        // Campo para el nombre completo
-        OutlinedTextField(
-            value = nombre,
-            onValueChange = {nombre = it; isNameError = false},
-            label = {Text("Nombre completo")},
-            modifier = Modifier.fillMaxWidth(),
-            isError = isNameError,
-            supportingText = {
-                if(isNameError){
-                Text(
-                    text = "Porfavor ingrese su nombre completo",
-                    color = MaterialTheme.colorScheme.error
+            //Campo para el nombre completo
+            OutlinedTextField(
+                value = nombre,
+                onValueChange = {nombre = it; isNameError = false},
+                label = {Text("Nombre completo")},
+                modifier = Modifier.fillMaxWidth(),
+                isError = isNameError,
+                supportingText = {
+                    if(isNameError){
+                        Text(
+                            text = "El nombre solo puede contener letras y espacios (sin números)",
+                            color = MaterialTheme.colorScheme.error
+                        )
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            //Campo del nombre del usuario
+            OutlinedTextField(
+                value = usuario,
+                onValueChange = {usuario = it; isUserError = false},
+                label = {Text("Nombre de Usuario")},
+                modifier = Modifier.fillMaxWidth(),
+                isError = isUserError,
+                supportingText = {
+                    if(isUserError) Text(
+                        text = "El usuario solo puede contener letras básicas, numeros y caracteres especiales (sin tildes, ñ o @)",
+                        color = MaterialTheme.colorScheme.error
                     )
                 }
-            }
-        )
+            )
 
-        Spacer(modifier = Modifier.height(12.dp))
-
-        //Campo del nombre del usuario
-        OutlinedTextField(
-            value = usuario,
-            onValueChange = {usuario = it; isUserError = false},
-            label = {Text("Nombre de Usuario")},
-            modifier = Modifier.fillMaxWidth(),
-            isError = isUserError,
-            supportingText = {
-                if(isUserError) Text(
-                    text = "Porfavor ingrese su nombre de usuario correctamente",
-                    color = MaterialTheme.colorScheme.error
-                )
-            }
-        )
-
-        Spacer(modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(12.dp))
 
 
-        //Campo para el correo
-        OutlinedTextField(
-            value = correo,
-            onValueChange = {correo = it; isEmailError = false},
-            label = {Text("Correo electrónico")},
-            modifier = Modifier.fillMaxWidth(),
-            isError = isEmailError,
-            supportingText = {
-                if(isEmailError) Text(
-                    text = "Correo Electornico invalido(ej:Alexis@ejemplo.com)",
-                    color = MaterialTheme.colorScheme.error
-                )
-            }
-        )
+            //Campo para el correo
+            OutlinedTextField(
+                value = correo,
+                onValueChange = {correo = it; isEmailError = false},
+                label = {Text("Correo electrónico")},
+                modifier = Modifier.fillMaxWidth(),
+                isError = isEmailError,
+                supportingText = {
+                    if(isEmailError) Text(
+                        text = "Correo Electornico invalido(ej:Alexis@ejemplo.com)",
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
+            )
 
-        Spacer(modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(12.dp))
 
-        //Campo para la Contraseña
-        OutlinedTextField(
-            value = contrasena,
-            onValueChange = {contrasena = it; isPasswordError = false},
-            label = {Text("Contraseña")},
-            visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth(),
-            isError = isPasswordError,
-            supportingText = {
-                if(isPasswordError) Text(
-                    text = "la contraseña debe ser maximo de 6 caracteres",
-                    color = MaterialTheme.colorScheme.error
-                )
-            }
-        )
+            //Campo para la Contraseña
+            OutlinedTextField(
+                value = contrasena,
+                onValueChange = {contrasena = it; isPasswordError = false; isConfirmPasswordError = false},
+                label = {Text("Contraseña")},
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth(),
+                isError = isPasswordError,
+                supportingText = {
+                    if(isPasswordError) Text(
+                        text = "Debe tener al menos 6 caracteres y no puede contener letras en español (tildes o ñ)",
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
+            )
 
-        Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.height(12.dp))
 
-        //Boton
-        Button(
-            onClick = validaryregistrar,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Registrar")
+            // NUEVO CAMPO: Confirmar Contraseña
+            OutlinedTextField(
+                value = confirmarContrasena,
+                onValueChange = {confirmarContrasena = it; isConfirmPasswordError = false},
+                label = {Text("Confirmar Contraseña")},
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth(),
+                isError = isConfirmPasswordError,
+                supportingText = {
+                    if(isConfirmPasswordError) {
+                        val message = when {
+                            confirmarContrasena.isEmpty() -> "Por favor, confirme su contraseña."
+                            !confirmarContrasena.matches(passwordRegex) -> "La contraseña no puede contener letras en español (tildes o ñ)."
+                            else -> "Las contraseñas no coinciden."
+                        }
+                        Text(text = message, color = MaterialTheme.colorScheme.error)
+                    }
+                }
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // Boton
+            Button(
+                onClick = validaryregistrar,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Registrar")
             }
         }
     }
